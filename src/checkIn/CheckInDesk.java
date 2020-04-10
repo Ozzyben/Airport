a2c25878ba70e3c17b6a9384295550dbeac91512
ITheCon <cksmith@net-ex.no>
2020-04-10 17:42:21 +0100

    Added commented out methods to checkInDesk class

--- src/checkIn/CheckInDesk.java
+++ src/checkIn/CheckInDesk.java
@@ -1,12 +1,13 @@
 package checkIn;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Queue;
 
 public class CheckInDesk extends Thread {
 
     private Queue<Passenger> queue;
-    private Passenger passenger;
+    private Passenger currentPassenger;
     private HashMap<String, Flight> planes;
     private CheckInGUI GUI;
 
@@ -15,13 +16,34 @@ public class CheckInDesk extends Thread {
         this.planes = planes;
         this.GUI = GUI;
     }
-
+/*    
+    private void addPassanger(Passenger passenger){
+    	if (queue.contains(passenger)){
+    		System.out.println("Passenger is already in the queue for this desk!");
+    	}else{
+    		queue.add(passenger);
+    	}
+    }
+*/
+/* 
+    private String createQueueList(){
+    	String passsengerList = "";
+    	if (!queue.isEmpty()){
+    		Iterator<Passenger> iterator = queue.iterator();
+    		while (iterator.hasNext()){
+    			Passenger passenger = iterator.next();
+    			passsengerList += passenger.getLastName() + "\n";
+    		}
+    	}
+    	return passsengerList;
+    }
+*/
     public void run() {
         while(!queue.isEmpty()) {
-            passenger = queue.remove();
-            System.out.println(passenger.getFlightCode());
-            String flightCode = passenger.getFlightCode();
-            planes.get(flightCode).addPassenger(passenger);
+            currentPassenger = queue.remove();
+            System.out.println(currentPassenger.getFlightCode());
+            String flightCode = currentPassenger.getFlightCode();
+            planes.get(flightCode).addPassenger(currentPassenger);
         }
     }
 
